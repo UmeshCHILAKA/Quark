716e8586100fe79c979ccf95417a719321a85927
Umesh CHILAKA <umesh.Chilaka@outlook.com>
2020-06-14 23:22:31 +0530

    Shield Class to check the method arguments

---
+++ Quark/Shield.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Quark
+{
+    /// <summary>
+    /// Helper functions to shield the methods with invalid arguments.
+    /// </summary>
+    public static class Shield
+    {
+        /// <summary>
+        /// Checks if specified parameter is not null. Throws ArgumentNullException, if parameter is null.
+        /// </summary>
+        /// <param name="paramName">Name of the parameter.</param>
+        /// <param name="argument">The argument that has to be checked for null.</param>
+        /// <exception cref="ArgumentNullException">Throws ArgumentNullException, if parameter is null</exception>
+        public static void IsNull(string paramName, object argument)
+        {
+            if (null == argument)
+                throw new ArgumentNullException(paramName);
+        }
+
+        /// <summary>
+        /// Checks if specified value is in limits. Throws ArgumentOutOfRangeException, if value is outside limits.
+        /// </summary>
+        /// <param name="value">The value to be checked.</param>
+        /// <param name="minimum">The minimum limit.</param>
+        /// <param name="maximum">The maximum limit.</param>
+        /// <param name="paramName">Name of the parameter being verified.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Throws ArgumentOutOfRangeException, if value is outside limits.</exception>
+        public static void IsOutofRange(double value, double minimum, double maximum, string paramName)
+        {
+            if (value < minimum || value > maximum)
+            {
+                throw new ArgumentOutOfRangeException(paramName);
+            }
+        }
+
+        /// <summary>
+        /// Checks if specified value is valid number. Throws ArgumentException, if value is NaN.
+        /// </summary>
+        /// <param name="paramName">Name of the parameter.</param>
+        /// <param name="value">The value to be checked for.</param>
+        /// <exception cref="ArgumentException">Throws ArgumentException, if value is NaN.</exception>
+        public static void IsNan(string paramName, double value)
+        {
+            if (double.IsNaN(value))
+                throw new ArgumentException(paramName);
+        }
+
+        /// <summary>hecks if specified value is valid number. Throws ArgumentException, if value is NaN or Infinity.</summary>
+        /// <param name="paramName">Name of the parameter.</param>
+        /// <param name="value">The value to be checked for.</param>
+        /// <exception cref="ArgumentException">Throws ArgumentException, if value is NaN or infinity</exception>
+        public static void IsValidNumber(string paramName, double value)
+        {
+            if (double.IsNaN(value) || double.IsInfinity(value))
+                throw new ArgumentException(paramName);
+        }
+
+        /// <summary>
+        /// Checks if Collection is empty. Throws ArgumentException, if Collection is empty.
+        /// </summary>
+        /// <param name="paramName">Name of the parameter.</param>
+        /// <param name="collection">The collection to be checked for.</param>
+        /// <exception cref="ArgumentException">Throws ArgumentException, if Collection is empty</exception>
+        /// TODO Edit XML Comment Template for IsCollectionEmpty
+        public static void IsCollectionEmpty(string paramName, ICollection collection)
+        {
+            IsNull(paramName, collection);
+
+            if (collection.Count == 0)
+                throw new ArgumentException(paramName);
+        }
+    }
+}
---
+++ Source/Quark.Tests/ShieldTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Quark.Tests
+{
+    [TestFixture()]
+    public class ShieldTests
+    {
+        const string DummyParameter = "DummyParam";
+        [Test()]
+        public void IsNullTest()
+        {
+            Shield.IsNull(DummyParameter, new object());
+            Assert.Throws<ArgumentNullException>(() => Shield.IsNull(DummyParameter, null));
+        }
+
+        [Test()]
+        public void IsOutOfRangeTests()
+        {
+            //Positive Numbers
+            Shield.IsOutofRange(5.0, 4.6, 5.5, DummyParameter);
+            Shield.IsOutofRange(5.0, 5.0, 5.5, DummyParameter);
+            Shield.IsOutofRange(5.0, 4.6, 5.0, DummyParameter);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => Shield.IsOutofRange(4.0, 4.6, 5.5, DummyParameter));
+            Assert.Throws<ArgumentOutOfRangeException>(() => Shield.IsOutofRange(4.599, 4.6, 5.5, DummyParameter));
+            Assert.Throws<ArgumentOutOfRangeException>(() => Shield.IsOutofRange(5.50001, 4.6, 5.5, DummyParameter));
+
+            //Negative Numbers
+            Shield.IsOutofRange(-5.0, -5.5, -4.6, DummyParameter);
+            Shield.IsOutofRange(-5.0, -5.5, -5.0, DummyParameter);
+            Shield.IsOutofRange(-5.0, -5.0, -4.6, DummyParameter);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => Shield.IsOutofRange(-4.0, -5.5, -4.6, DummyParameter));
+            Assert.Throws<ArgumentOutOfRangeException>(() => Shield.IsOutofRange(-4.599, -5.5, -4.6, DummyParameter));
+            Assert.Throws<ArgumentOutOfRangeException>(() => Shield.IsOutofRange(-5.50001, -5.5, -4.6, DummyParameter));
+
+        }
+
+        [Test()]
+        public void IsNanTests()
+        {
+            Shield.IsNan(DummyParameter, 88.28);
+            Shield.IsNan(DummyParameter, double.NegativeInfinity);
+            Shield.IsNan(DummyParameter, double.PositiveInfinity);
+
+            Assert.Throws<ArgumentException>(() => Shield.IsNan(DummyParameter, double.NaN));
+
+        }
+
+        [Test()]
+        public void IsValidNumberTestes()
+        {
+            Shield.IsValidNumber(DummyParameter, 11.57);
+
+            Assert.Throws<ArgumentException>(() => Shield.IsValidNumber(DummyParameter, double.NegativeInfinity));
+            Assert.Throws<ArgumentException>(() => Shield.IsValidNumber(DummyParameter, double.PositiveInfinity));
+            Assert.Throws<ArgumentException>(() => Shield.IsNan(DummyParameter, double.NaN));
+        }
+
+        [Test()]
+        public void IsCollectionEmptyTests()
+        {
+            Shield.IsCollectionEmpty(DummyParameter, new List<int> { 6 });
+
+            Assert.Throws<ArgumentNullException>(() => Shield.IsCollectionEmpty(DummyParameter, null));
+            Assert.Throws<ArgumentException>(() => Shield.IsCollectionEmpty(DummyParameter, new List<int>()));
+        }
+    }
+}
