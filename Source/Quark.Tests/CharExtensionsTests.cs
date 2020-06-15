e53073234dff3709e67ff8cca460dceffe51a686
Umesh CHILAKA <umesh.Chilaka@outlook.com>
2020-06-15 23:02:05 +0530

    Char Extensions added

---
+++ Source/Quark.Tests/CharExtensionsTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+
+namespace Quark.Tests
+{
+    [TestFixture()]
+    public class CharExtensionsTests
+    {
+        [Test]
+        public void GetUnicodeValueTest()
+        {
+            Assert.AreEqual("U+0061", 'a'.ToUnicodeValue());
+            Assert.AreEqual("U+0009", '\t'.ToUnicodeValue());
+            Assert.AreEqual("U+0020", ' '.ToUnicodeValue());
+            Assert.AreEqual("U+0041", 'A'.ToUnicodeValue());
+            Assert.AreEqual("U+005C", '\\'.ToUnicodeValue());
+        }
+
+        [Test]
+        public void ToUrlValueTest()
+        {
+            Assert.AreEqual("a", 'a'.ToUrlValue());
+            Assert.AreEqual("A", 'A'.ToUrlValue());
+
+            Assert.AreEqual("\\", '\\'.ToUrlValue());
+            Assert.AreEqual("/", '/'.ToUrlValue());
+
+            //Test for IsControl
+            Assert.AreEqual("%09", '\t'.ToUrlValue());
+            Assert.AreEqual("%07", '\a'.ToUrlValue());
+            Assert.AreEqual("%00", '\0'.ToUrlValue());
+
+            //Test for IsSeparator
+            Assert.AreEqual("%20", ' '.ToUrlValue());
+            Assert.AreEqual("%0D", '\r'.ToUrlValue());
+            Assert.AreEqual("%0A", '\n'.ToUrlValue());
+
+            //Test for IsSurrogate
+            const string str = "\U00010F00";
+            Assert.AreEqual("%D803", str[0].ToUrlValue());
+        }
+
+        [Test]
+        public void DisplayValueTest()
+        {
+            Assert.AreEqual("a", 'a'.ToDisplayValue());
+            Assert.AreEqual("A", 'A'.ToDisplayValue());
+            Assert.AreEqual("\\", '\\'.ToDisplayValue());
+
+            //Test for IsControl
+            Assert.AreEqual("U+0009", '\t'.ToDisplayValue());
+            Assert.AreEqual("U+0007", '\a'.ToDisplayValue());
+            Assert.AreEqual("U+0000", '\0'.ToDisplayValue());
+
+            //Test for IsSeparator
+            Assert.AreEqual("U+0020", ' '.ToDisplayValue());
+            Assert.AreEqual("U+000D", '\r'.ToDisplayValue());
+            Assert.AreEqual("U+000A", '\n'.ToDisplayValue());
+
+            //Test for IsSurrogate
+            const string str = "\U00010F00";
+            Assert.AreEqual("U+D803", str[0].ToDisplayValue());
+        }
+    }
+}
---
+++ Source/Quark/CharExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+
+namespace Quark
+{
+    /// <summary>
+    /// Helper class for char operations
+    /// </summary>
+    public static class CharExtensions
+    {
+        /// <summary>
+        /// Returns the Unicode value of the character.
+        /// </summary>
+        /// <param name="character">The character that has to be converted.</param>
+        /// <returns>Unicode value</returns>
+        public static string ToUnicodeValue(this char character)
+        {
+            return string.Format(CultureInfo.InvariantCulture, "U+{0:X4}", (int)character);
+        }
+
+        /// <summary>
+        /// Returns displayable value for a character.
+        /// </summary>
+        /// <param name="character">The character that has to be checked.</param>
+        /// <returns>Displayable string for the character</returns>
+        public static string ToDisplayValue(this char character)
+        {
+            if (char.IsControl(character) || char.IsSeparator(character) ||
+                char.IsSurrogate(character))
+                return ToUnicodeValue(character);
+
+            return string.Format(CultureInfo.InvariantCulture, "{0}", character);
+        }
+
+        /// <summary>
+        /// Returns the URL equivalent character.
+        /// </summary>
+        /// <param name="character">The character that has to be checked.</param>
+        /// <returns>Character equivalent that has to be used in URL</returns>
+        public static string ToUrlValue(this char character)
+        {
+            //TODO: Needs to be checked for  char.IsSymbol
+            //TODO: Check Path.InvalidPathChars.
+
+            if (char.IsControl(character) || char.IsSeparator(character) ||
+                char.IsSurrogate(character))
+                return string.Format(CultureInfo.InvariantCulture, "%{0:X2}", (int)character);
+
+            return string.Format(CultureInfo.InvariantCulture, "{0}", character);
+        }
+    }
+}
