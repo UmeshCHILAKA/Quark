4f6e976fa2f391462a0e90c3d6d07d2b1be30ca1
Umesh CHILAKA <umesh.Chilaka@outlook.com>
2020-06-16 22:13:11 +0530

    Corrected Unit test to Pass build actions

--- Source/Quark.Tests/StringExtensionsTests.cs
+++ Source/Quark.Tests/StringExtensionsTests.cs
@@ -8,7 +8,7 @@ namespace Quark.Tests
         [Test()]
         public void LimitCharsTest()
         {
-            const string OrgString = "99912345678901234567890";
+            const string OrgString = "12345678901234567890";
             const string stringAll = OrgString;
             const string string15 = "123456789012...";
             const string string5 = "12...";
